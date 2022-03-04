2cd90b0566b9e75d0cc5caeb5bd64ea8c7c0d8aa
jjjjjjji-hun <lingcke@naver.com>
2022-03-04 15:53:48 +0900

    서블릿 추가

--- 20220209(김지훈)자바연동.sql
+++ 20220209(김지훈)자바연동.sql
@@ -21,3 +21,21 @@ INSERT INTO userinfo VALUES ('장부트', 'JB', 'bootJang', null);
 
 SELECT * FROM userinfo;
 DELETE FROM userinfo WHERE uid = 'JB';
+
+-- 게시판 테이블(boardinfo)을 생성해보겠습니다.
+CREATE TABLE boardinfo(
+board_num INT PRIMARY KEY AUTO_INCREMENT,
+title VARCHAR(100) NOT NULL,
+content VARCHAR(2000) NOT NULL,
+writer VARCHAR(30) NOT NULL,
+bdate DATETIME DEFAULT now(),
+mdate DATETIME DEFAULT now(),
+hit INT DEFAULT 0
+);
+
+SELECT * FROM boardinfo;
+
+INSERT INTO boardinfo(title, content, writer) VALUES('jsp 배우기', 'jsp는 배울게 진짜 많구나 열심히 배워야지', '김자바');
+INSERT INTO boardinfo VALUES(null, 'html', 'html도 빨리 복습해야하는데', '최스프링', now(), now(), 0);
+INSERT INTO boardinfo(title, content, writer) VALUES('java', 'java 까먹지 않았겠지 걱정이다', '박파이썬');
+
